0870fcd534c6367c48b7b8b1034b961da42686b4
shubham-affinsys <shubham.chauhan@affinsys.com>
2024-11-18 11:35:27 +0530

    fix: logging error

--- svc/log.py
+++ svc/log.py
@@ -1,7 +1,7 @@
 import logging
 
 logger = logging.getLogger('survey-builder')
-logger.setLevel(logging.info)
+logger.setLevel(logging.INFO)
 handler = logging.StreamHandler()
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
